653e4b28f9d4910333b62a2c6f100a7d29792b5d
Kaylee Martynow <kayleemartynow@creighton.edu>
2021-09-22 15:05:53 -0500

    stringupdates

---
+++ forms/calc/calc.js
@@ -0,0 +1,14 @@
+/*
+
+function  calculate(num1, num2) {
+  summedNumbers = num1+num2
+return summedNumbers
+}
+
+let num1 = Number (prompt ("Insert a number:"))
+let num2 = Number( prompt ("Insert a number:"))
+
+let answer = calculate (num1, num2)
+console.log(`The sum of ${num1} and ${num2} is ${answer}`)
+
+*/
---
+++ forms/while/while.js
@@ -0,0 +1,8 @@
+/* let i = 1
+
+while ( i <= 10) {
+  console.log(`This is loop interation ${i}`)
+  i = i +1
+  } 
+  
+  */
